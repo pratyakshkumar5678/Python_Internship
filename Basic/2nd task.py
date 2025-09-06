a12e2b023e38fb1e5fd6e65cd357dd1e3c921bfc
Pratyaksh Kumar <pratyakshkumar5678@gmail.com>
2025-09-07 03:27:35 +0530

    2nd task  completd

--- Basic/2nd task.py
+++ Basic/2nd task.py
@@ -2,4 +2,25 @@ import random
 guess_number=random.randint(1,100)
 max_attempts=5
 attempts=0
-
+print("Welcome to the number guessing game.\n")
+print("Rules are simple:\n1.)Guess a number from 1 to 100.\n2.) You only get 5 chances. ")
+while attempts < max_attempts:
+    guess=int(input(f"Attempt {attempts+1}/{max_attempts}: "))
+    if guess==guess_number and attempts==0:
+        print(f"Are you Human. You guessed it in {attempts+1} attempt.")
+        break
+    elif guess==guess_number and attempts>=2 and attempts<max_attempts:
+        print(f"Congratulations! You guessed the number in {attempts+1} attempt.")
+        break
+    elif guess<(guess_number-10):
+        print("Guessed number is too low.")
+    elif (guess_number-10)<guess<guess_number:
+        print("Close but slightly higher.")
+    elif (guess_number+10)<guess:
+        print("Guessed number is too high.")
+    else:
+        print("Close but slightly lower.")
+    attempts+=1
+    if guess!=guess_number and attempts==max_attempts:
+        print(f"Sorry, out of chances. The correct number to guess was {guess_number}.")
+        
