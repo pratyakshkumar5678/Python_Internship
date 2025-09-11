c0efecd41df5d160c876db79a6c52a5d469bea74
Pratyaksh Kumar <pratyakshkumar5678@gmail.com>
2025-09-11 23:59:21 +0530

    Update 3rd Task.py

---
+++ Basic/3rd Task.py
@@ -0,0 +1,16 @@
+def word_counter(filename):
+    try:
+        with open(filename,'r') as OpenedFile:
+            WordCount=0
+            for items in OpenedFile:
+                words=items.split()
+                WordCount+=len(words)
+            else:
+                return WordCount
+    except FileNotFoundError:
+        print(f"The file {filename} was not found.")
+        return 0
+file=(input("Enter name of the file (with extension): "))
+total_words=word_counter(file)
+if word_counter==True:
+    print(f"THe total number of words in {file} is: {total_words}")
